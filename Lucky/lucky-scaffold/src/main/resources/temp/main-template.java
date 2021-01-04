08d9a2528645082453557db6bbf965ab4c8ca029
FK7075 <1814375626@qq.com>
2021-01-05 01:40:09 +0800

    2021-01-05 1：33 lucky-web-management找到最新的模版

--- Lucky/lucky-scaffold/src/main/resources/temp/main-template.java
+++ Lucky/lucky-scaffold/src/main/resources/temp/main-template.java
@@ -1,13 +1,15 @@
 package @:Package;
 
-import com.lucky.boot.startup.LuckyBootApplication;
+import com.lucky.boot.startup.LuckyApplication;
+import com.lucky.framework.annotation.LuckyBootApplication;
 
 /**
  * 启动类
  */
+@LuckyBootApplication
 public class @:MainName {
 
     public static void main( String[] args ) {
-        LuckyBootApplication.run(@:MainName.class,args);
+        LuckyApplication.run(@:MainName.class,args);
     }
 }
